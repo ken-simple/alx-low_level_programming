a9850e0dcf12feaecda61d99a597d3b2afcf7ba9
ken-simple <kensimple56@yahoo.com>
2023-10-08 15:24:13 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
+/**
+ * main - printing a line using the function puts
+ * return: 0 (success)
+ */
 int main(void)
 {
-	puts("\programming is like building a multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
 }
