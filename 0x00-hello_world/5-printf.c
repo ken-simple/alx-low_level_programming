8cde3109262f71b055c044275cb322d83fcce690
ken-simple <kensimple56@yahoo.com>
2023-10-08 16:03:53 -0700

    hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#inlcude < stdio.h >
+/**
+ * main - this is a prinf function
+ * Return: 0
+ */
+int main(void)
+{
+	print("with proper grammar, but the outcome is a piece of art,\n"
+	return (0);
+}
