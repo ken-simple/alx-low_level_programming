559a0d483dd1b096a03a2b01f9c7d9894b021854
ken-simple <kensimple56@yahoo.com>
2023-10-20 03:59:17 -0700

    larget number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,26 +1,12 @@
 #ifndef MAIN_H
+
 #define MAIN_H
 
 #include <stdio.h>
 
 void positive_or_negative(int i);
-int largest_number(int a, int b, int c)
-{
-    int largest;
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
 
-    if (a > b && b > c)
-    {
-        largest = a;
-    }
-    else if (b > a && a > c)
-    {
-        largest = b;
-    }
-    else
-    {
-        largest = c;
-    }
-
-    return (largest);
-}
 #endif /* MAIN_H */
